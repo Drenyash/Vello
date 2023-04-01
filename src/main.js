673ec762f4425b4b2e86dad2817c80dde58f864e
TakenHara <kavlod12345@gmail.com>
2023-04-01 13:20:35 +0200

    init: start prod

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize-scss'
+import './helpers/css/style.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(store).use(router).mount('#app')
